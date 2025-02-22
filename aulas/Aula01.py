03cdc3e3f9c79cc47fd34459750fc22581dacf97
lucassilva232902 <lucasall263@gmail.com>
2025-02-22 09:04:28 -0300

    aula 1
    8/2

---
+++ aulas/Aula01.py
@@ -0,0 +1,46 @@
+print('Hello World!')
+
+# comentário de uma linha
+'''
+comentário
+de
+múltiplas
+linhas
+'''
+
+print('Olá!')
+print('Olá!')
+
+'''
+Operações Matemáticas
+Adição → +
+Subtração → -
+Divisão → /
+Multiplicação → *
+Exponencial → **
+
+Ordem das operações em Python igual na matemática
+2 + 10 * 10 - 5
+
+Voce pode notação científica para especificar ordem
+(2+10) * (10-5)
+'''
+
+'''Variáveis
+
+São formas de salvar dados no computador
+
+numero = 10
+numero2 = 90
+
+resultado = numero + numero2
+
+regras pra declarar variável
+
+1ª → toda varável tem nome único
+2ª → os nomes não podem começar com números EX.: 2numero
+3ª → não pode haver espaços no nome da variável EX.: nome completo = errado / nome_completo = certo / nomeCompleto
+4ª → não pode ter caractere especial Ex.: '", <> /? | \ ()! @ # $% ^ & * ~ - +
+
+5ª (opicional) → começar com letra minúscula
+'''
